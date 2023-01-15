7b5ea79db7d4f5aafab7f276a582ad784e9870fa
Simon Cateau <sorani.dev@outlook.com>
2023-01-15 23:25:40 +0100

    Add and update templates

--- 00-default_template_ttkbootstrap.py
+++ 00-default_template_ttkbootstrap.py
@@ -1,15 +1,17 @@
 from pathlib import Path
 from tkinter import Tk
+from ttkbootstrap.constants import *
+import ttkbootstrap as tb
 
 # Variables 
 # Global Constants
 BASE_PATH: Path = Path(__file__).parent.resolve()
 TITLE:str = "" 
-DIMENSIONS = "500x700"
+DIMENSIONS = "500x350"
 
 # Make app
-root:Tk = Tk()
-root.title(f"Simon Cateau Tkinter - {TITLE}")
+root:Tk = tb.Window(themename="superhero")
+root.title(f"TTk Bootstrap - {TITLE}")
 root.iconbitmap(Path(BASE_PATH, "images", "codemy.ico").resolve())
 root.geometry(DIMENSIONS)
 
